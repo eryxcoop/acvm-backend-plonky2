40b526e4031dae0047eac2a20241f3c46d6f8cb9
bruno <bweisz@eryxsoluciones.com.ar>
2024-05-09 16:15:22 -0300

    Extract things

--- plonky2-backend/src/main.rs
+++ plonky2-backend/src/main.rs
@@ -7,19 +7,22 @@ use acir::circuit::Program;
 use acir::native_types::WitnessStack;
 // use serde::Deserialize;
 
-fn deserialize_program_within_file_path(acir_program_path: &String) -> Program {
-    let mut file = File::open(acir_program_path).expect("There was a problem reading the file");
+fn read_file_to_bytes(file_path: &String) -> Vec<u8> {
+    let mut file = File::open(file_path).expect("There was a problem reading the file");
     let mut buffer: Vec<u8> = Vec::new();
     let _ = file.read_to_end(&mut buffer);
+    return buffer
+}
+
+fn deserialize_program_within_file_path(acir_program_path: &String) -> Program {
+    let mut buffer = read_file_to_bytes(acir_program_path);
     let file_contents_slice: &[u8] = &buffer;
     let program = Program::deserialize_program(file_contents_slice);
     program.unwrap()
 }
 
 fn deserialize_witnesses_within_file_path(witnesses_path: &String) -> WitnessStack {
-    let mut file = File::open(witnesses_path).expect("There was a problem reading the file");
-    let mut buffer: Vec<u8> = Vec::new();
-    let _ = file.read_to_end(&mut buffer);
+    let mut buffer = read_file_to_bytes(witnesses_path);
     let file_contents_slice: &[u8] = &buffer;
     let witness_stack = WitnessStack::try_from(file_contents_slice);
     witness_stack.unwrap()

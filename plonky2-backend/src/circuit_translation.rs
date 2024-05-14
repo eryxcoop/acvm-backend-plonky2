c9ca3b933f5040a4fce5f5a49470a2ee88259730
bruno <bweisz@eryxsoluciones.com.ar>
2024-05-14 16:18:55 -0300

    Test 2 passing with Prime Field change to noir code, moving on to complete the assertZero opcode

--- plonky2-backend/src/circuit_translation.rs
+++ plonky2-backend/src/circuit_translation.rs
@@ -23,27 +23,52 @@ type C = KeccakGoldilocksConfig;
 type F = <C as GenericConfig<D>>::F;
 type CB = CircuitBuilder::<F, D>;
 
+fn field_element_to_goldilocks_field(fe: &FieldElement) -> GoldilocksField {
+    // let goldilocks_prime: u64 = 18446744069414584321;
+    // let goldilicks_prime_as_big_uint = BigUint::from_bytes_be(&goldilocks_prime.to_be_bytes());
+    // let fe_as_big_uint = BigUint::from_bytes_be(&fe.to_be_bytes() as &[u8]);
+    // if fe_as_big_uint >= goldilicks_prime_as_big_uint {
+    //     let inverted_fe = fe;
+    //     let bui = BigUint::from_bytes_be(&inverted_fe.to_be_bytes());
+    //     let inverted_g = GoldilocksField::from_noncanonical_biguint(bui);
+    //     return -inverted_g;
+    // }
+    // let bui = BigUint::from_bytes_be(&fe.to_be_bytes());
+    // GoldilocksField::from_noncanonical_biguint(bui)
+
+    let fe_as_big_uint = BigUint::from_bytes_be(&fe.to_be_bytes() as &[u8]);
+    GoldilocksField::from_noncanonical_biguint(fe_as_big_uint)
+}
+
 fn translate_assert_zero(builder: &mut CB, expression: &Expression, witness_target_map: &mut HashMap<Witness, Target>) {
     println!("{:?}", expression);
     assert_eq!(0, expression.mul_terms.len());
-    assert_eq!(FieldElement::zero(), expression.q_c);
     let linear_combinations = &expression.linear_combinations;
     assert_eq!(1, linear_combinations.len());
-    let (field_element, witness) = &linear_combinations[0];
+
+    let (f_single_multiply_factor, witness) = &linear_combinations[0];
 
     let target = if let Some(target) = witness_target_map.get(witness) {
         *target
     } else {
-        todo!();
-
-        // let new_target = builder.add_virtual_target();
-        // witness_target_map.insert(*witness, new_target);
-        // new_target
+        let new_target = builder.add_virtual_target();
+        witness_target_map.insert(*witness, new_target);
+        new_target
     };
 
-    let g_field_element= GoldilocksField::from_canonical_u64(1);
+    // let target_constant = builder.constant(g_constant);
+    // let g_single_multiply_factor = field_element_to_goldilocks_field(f_single_multiply_factor); // Hardcodeado el factor 1
+    // let g_zero = GoldilocksField::default();
+    // let result_target = builder.mul_const(g_single_multiply_factor, target);
+    // let result_target = builder.arithmetic(
+    //     g_single_multiply_factor,
+    //     g_zero,
+    //     target,
+    //     target,
+    //     target_constant);
 
-    let result_target = builder.mul_const(g_field_element, target);
+    let g_constant = field_element_to_goldilocks_field(&expression.q_c);
+    let result_target = builder.add_const(target, g_constant);
     builder.assert_zero(result_target);
 }
 
@@ -52,11 +77,12 @@ fn generate_plonky2_circuit_from_acir_circuit(circuit: &Circuit) -> (CircuitData
     let config = CircuitConfig::standard_recursion_config();
     let mut builder = CB::new(config);
 
-    let public_input_1 = circuit.public_parameters.0.first().unwrap();
-    let target_1 = builder.add_virtual_target();
-    builder.register_public_input(target_1);
+    let public_input = circuit.public_parameters.0.first().unwrap();
+    let public_input_target = builder.add_virtual_target();
+    builder.register_public_input(public_input_target);
+
     let mut witness_target_map: HashMap<Witness, Target> = HashMap::new();
-    witness_target_map.insert(*public_input_1, target_1);
+    witness_target_map.insert(*public_input, public_input_target);
 
     for opcode in &circuit.opcodes {
         match opcode {
@@ -78,11 +104,11 @@ mod tests {
     use plonky2::iop::witness::WitnessWrite;
     use super::*;
     #[test]
-    fn test_plonky2_vm_can_traslate_the_most_basic_assert_zero_program(){
+    fn test_plonky2_vm_can_traslate_the_assert_x_equals_zero_program(){
         // Given
         let public_input_witness = Witness(0);
-        let only_expr = x_equals_0_opcode(public_input_witness);
-        let circuit = circuit_with_single_opcode(only_expr, public_input_witness);
+        let only_opcode = x_equals_0_opcode(public_input_witness);
+        let circuit = circuit_with_single_opcode(only_opcode, public_input_witness);
 
         // When
         let (circuit_data, witness_target_map) = generate_plonky2_circuit_from_acir_circuit(&circuit);
@@ -117,31 +143,35 @@ mod tests {
         })
     }
 
+    #[test]
+    fn test_plonky2_vm_can_traslate_the_assert_x_equals_constant_program(){
+        // Given
+        let public_input_witness = Witness(0);
+        let only_opcode = x_equals_4_opcode(public_input_witness);
+        let circuit = circuit_with_single_opcode(only_opcode, public_input_witness);
+
+        // When
+        let (circuit_data, witness_target_map) = generate_plonky2_circuit_from_acir_circuit(&circuit);
+
+        // Then
+        let mut witnesses = PartialWitness::<GoldilocksField>::new();
+        let four = GoldilocksField::from_canonical_u64(4);
+        let public_input_plonky2_target = witness_target_map.get(&public_input_witness).unwrap();
+        witnesses.set_target(*public_input_plonky2_target, four);
+        let proof = circuit_data.prove(witnesses).unwrap();
+        assert_eq!(four, proof.public_inputs[0]);
+    }
+
+    fn x_equals_4_opcode(public_input_witness: Witness) -> Opcode {
+        AssertZero(Expression {
+            mul_terms: Vec::new(),
+            linear_combinations: vec![(FieldElement::one(), public_input_witness)],
+            q_c: -FieldElement::from_hex("0x04").unwrap()
+        })
+    }
+
     // #[test]
-    // fn test_plonky2_vm_can_translate_an_assert_zero_program_with_constant_multiplication(){
-    //     // Given
-    //     let public_input_witness = Witness(0);
-    //     let only_expr = Expression {mul_terms: Vec::new(),
-    //         linear_combinations: vec![(FieldElement::from_hex("0x04").unwrap(), public_input_witness)],
-    //         q_c: FieldElement::zero() };
-    //
-    //     let circuit = Circuit {
-    //         current_witness_index: 0,
-    //         expression_width: ExpressionWidth::Unbounded,
-    //         opcodes: vec![AssertZero(only_expr)],
-    //         private_parameters: BTreeSet::new(),
-    //         public_parameters: PublicInputs(BTreeSet::from_iter(vec![public_input_witness])),
-    //         return_values: PublicInputs(BTreeSet::new()),
-    //         assert_messages: Default::default(),
-    //         recursive: false,
-    //     };
-    //
-    //     // When
-    //     let proof = generate_plonky2_circuit_from_acir_circuit(&circuit);
-    //
-    //     // Then
-    //
-    // }
+    // fn test_plonky2_vm_can_traslate_the_assert_c_times_x_equals_constant_program() {}
 
     #[test]
     fn test_xxxxxx() {

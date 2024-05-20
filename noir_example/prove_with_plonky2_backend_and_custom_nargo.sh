320e704007743f5b6901708553ebacb9f84cca22
Caro Lang <clang@eryx.co>
2024-05-20 13:33:47 -0300

    Use custom nargo to make nargo check in script

--- noir_example/prove_with_plonky2_backend_and_custom_nargo.sh
+++ noir_example/prove_with_plonky2_backend_and_custom_nargo.sh
@@ -1,3 +1,3 @@
 cargo build --manifest-path ../noir/Cargo.toml
-nargo check
+../noir/target/debug/nargo check
 NARGO_BACKEND_PATH=~/.nargo/backends/acvm-backend-plonky2/backend_binary ../noir/target/debug/nargo prove

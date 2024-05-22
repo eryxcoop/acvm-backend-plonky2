69efffbbfcc57835ab301d6a5226c1090b79f657
bruno <bweisz@eryxsoluciones.com.ar>
2024-05-22 17:01:39 -0300

    Adds comment

--- plonky2-backend/src/circuit_translation/assert_zero_translator.rs
+++ plonky2-backend/src/circuit_translation/assert_zero_translator.rs
@@ -1,5 +1,7 @@
 use super::*;
 
+// This is a planned refactor, currently this functionality is implemented in the
+// CircuitBuilderFromAcirToPlonky2 struct
 struct AssertZeroTranslator {
     builder: CB,
     witness_target_map: HashMap<Witness, Target>,

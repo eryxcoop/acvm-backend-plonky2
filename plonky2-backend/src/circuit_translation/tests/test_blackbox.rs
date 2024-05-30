f3746b7f4b04a072bc6ca7e16c6d12bf1648e09f
bruno <bweisz@eryxsoluciones.com.ar>
2024-05-30 17:02:09 -0300

    Tests for u64 range check

--- plonky2-backend/src/circuit_translation/tests/test_blackbox.rs
+++ plonky2-backend/src/circuit_translation/tests/test_blackbox.rs
@@ -36,6 +36,32 @@ fn test_backend_cannot_provide_witness_value_bigger_than_u16_for_u16_range_check
     test_range_check_with_witness_value(max_allowed_witness_value_field, max_num_bits);
 }
 
+#[test]
+fn test_backend_can_translate_blackbox_func_call_range_check_u32() {
+    let max_num_bits = 32;
+    let max_allowed_witness_value = 2u64.pow(max_num_bits.clone()) - 1;
+    let max_allowed_witness_value_field = F::from_noncanonical_u64(max_allowed_witness_value.into());
+    test_range_check_with_witness_value(max_allowed_witness_value_field, max_num_bits);
+}
+
+#[test]
+#[should_panic]
+fn test_backend_cannot_provide_witness_value_bigger_than_u32_for_u32_range_check() {
+    let max_num_bits = 32;
+    let min_not_allowed_witness_value = 2u64.pow(max_num_bits.clone());
+    let max_allowed_witness_value_field = F::from_noncanonical_u64(min_not_allowed_witness_value.into());
+    test_range_check_with_witness_value(max_allowed_witness_value_field, max_num_bits);
+}
+
+// #[test]
+// #[should_panic]
+// fn test_backend_does_not_support_range_check_for_u64_or_bigger() {
+//     let max_num_bits = 16;
+//     let min_not_allowed_witness_value = 2u32.pow(max_num_bits.clone());
+//     let max_allowed_witness_value_field = F::from_noncanonical_u64(min_not_allowed_witness_value.into());
+//     test_range_check_with_witness_value(max_allowed_witness_value_field, max_num_bits);
+// }
+
 fn test_range_check_with_witness_value(witness_value: F, max_num_bits: u32){
     //Given
     let public_input_witness = Witness(0);

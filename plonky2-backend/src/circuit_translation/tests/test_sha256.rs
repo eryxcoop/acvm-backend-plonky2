321dfae304fdf30a6968fcac08bfc18193a7ff2f
bruno <bweisz@eryxsoluciones.com.ar>
2024-07-15 11:08:13 -0300

    test rotate right passing

--- plonky2-backend/src/circuit_translation/tests/test_sha256.rs
+++ plonky2-backend/src/circuit_translation/tests/test_sha256.rs
@@ -1,19 +1,47 @@
+use plonky2::iop::witness::{PartialWitness, WitnessWrite};
+
 use crate::circuit_translation::binary_digits_target::BinaryDigitsTarget;
-use crate::circuit_translation::CircuitBuilderFromAcirToPlonky2;
+
 use super::*;
 
 // These are unit tests for internal functions of the sha256 algorithm
 // They are agnostic to acir code
 
+#[test]
 fn test_rotate_right_1(){
     // pub fn rotate_right(
-    //     &mut self,
     //     binary_target: &BinaryDigitsTarget,
     //     times: usize,
+    //     builder: CB
     // ) -> BinaryDigitsTarget {
-    let circuit_translator = CircuitBuilderFromAcirToPlonky2::new();
+    let config = CircuitConfig::standard_recursion_config();
+    let mut circuit_builder = CB::new(config);
 
-    // let bits = vec![circuit_translator.builder.add_virtual_bool_target_unsafe()]
-    // let binary_input = BinaryDigitsTarget{bits: bits};
+    let bits = vec![
+        circuit_builder.add_virtual_bool_target_unsafe(),
+        circuit_builder.add_virtual_bool_target_unsafe(),
+        circuit_builder.add_virtual_bool_target_unsafe(),
+        circuit_builder.add_virtual_bool_target_unsafe(),
+    ];
+    let binary_input = BinaryDigitsTarget{bits};
+    let rotated_bits = BinaryDigitsTarget::rotate_right(&binary_input, 1, &mut circuit_builder);
+
+    let g_zero = F::default();
+    let g_one = F::from_canonical_u32(1);
+
+    let mut partial_witnesses = PartialWitness::<F>::new();
+    partial_witnesses.set_target(binary_input.bits[0].target, g_zero);
+    partial_witnesses.set_target(binary_input.bits[1].target, g_zero);
+    partial_witnesses.set_target(binary_input.bits[2].target, g_one);
+    partial_witnesses.set_target(binary_input.bits[3].target, g_zero);
+
+    partial_witnesses.set_target(rotated_bits.bits[0].target, g_zero);
+    partial_witnesses.set_target(rotated_bits.bits[1].target, g_zero);
+    partial_witnesses.set_target(rotated_bits.bits[2].target, g_zero);
+    partial_witnesses.set_target(rotated_bits.bits[3].target, g_one);
+
+    let circuit_data =circuit_builder.build::<C>();
+    let proof = circuit_data.prove(partial_witnesses).unwrap();
+    assert!(circuit_data.verify(proof).is_ok());
 
 }

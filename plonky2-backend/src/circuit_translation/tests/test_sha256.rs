075016f0c6f4ec99572b524ae24482d0ae7fe47f
bruno <bweisz@eryxsoluciones.com.ar>
2024-07-15 11:39:28 -0300

    Tests for shift right

--- plonky2-backend/src/circuit_translation/tests/test_sha256.rs
+++ plonky2-backend/src/circuit_translation/tests/test_sha256.rs
@@ -73,6 +73,34 @@ fn test_rotate_right_32_32(){
     test_rotate_right(32, 32, inputs, outputs);
 }
 
+#[test]
+fn test_shift_right_4_1(){
+    let g_zero = F::default();
+    let g_one = F::from_canonical_u32(1);
+    let inputs = vec![g_one, g_one, g_one, g_one];
+    let outputs = vec![g_zero, g_one, g_one, g_one];
+    test_shift_right(1, 4, inputs, outputs);
+}
+
+#[test]
+fn test_shift_right_32_16(){
+    let g_zero = F::default();
+    let g_one = F::from_canonical_u32(1);
+    let inputs = vec![
+        g_one, g_one, g_one, g_one, g_one, g_one, g_one, g_one,
+        g_one, g_one, g_one, g_one, g_one, g_one, g_one, g_one,
+        g_zero, g_zero, g_zero, g_zero, g_zero, g_zero, g_zero, g_zero,
+        g_zero, g_zero, g_zero, g_zero, g_zero, g_zero, g_zero, g_zero,
+    ];
+    let outputs = vec![
+        g_zero, g_zero, g_zero, g_zero, g_zero, g_zero, g_zero, g_zero,
+        g_zero, g_zero, g_zero, g_zero, g_zero, g_zero, g_zero, g_zero,
+        g_one, g_one, g_one, g_one, g_one, g_one, g_one, g_one,
+        g_one, g_one, g_one, g_one, g_one, g_one, g_one, g_one,
+    ];
+    test_shift_right(16, 32, inputs, outputs);
+}
+
 
 fn test_rotate_right(n: usize, size: usize, input_values: Vec<F>, output_values: Vec<F>){
     let config = CircuitConfig::standard_recursion_config();
@@ -82,6 +110,26 @@ fn test_rotate_right(n: usize, size: usize, input_values: Vec<F>, output_values:
 
     let binary_input = BinaryDigitsTarget{bits};
     let rotated_bits = BinaryDigitsTarget::rotate_right(&binary_input, n, &mut circuit_builder);
+
+    let mut partial_witnesses = PartialWitness::<F>::new();
+    for i in 0..size {
+        partial_witnesses.set_target(binary_input.bits[i].target, input_values[i]);
+        partial_witnesses.set_target(rotated_bits.bits[i].target, output_values[i]);
+    }
+
+    let circuit_data =circuit_builder.build::<C>();
+    let proof = circuit_data.prove(partial_witnesses).unwrap();
+    assert!(circuit_data.verify(proof).is_ok());
+}
+
+fn test_shift_right(n: usize, size: usize, input_values: Vec<F>, output_values: Vec<F>){
+    let config = CircuitConfig::standard_recursion_config();
+    let mut circuit_builder = CB::new(config);
+
+    let bits = (0..size).into_iter().map(|_| circuit_builder.add_virtual_bool_target_unsafe()).collect();
+
+    let binary_input = BinaryDigitsTarget{bits};
+    let rotated_bits = BinaryDigitsTarget::shift_right(&binary_input, n, &mut circuit_builder);
 
     let mut partial_witnesses = PartialWitness::<F>::new();
     for i in 0..size {

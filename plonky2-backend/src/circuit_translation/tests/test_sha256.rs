47b5eef9963f41ce66de0c17abf406e85dc8dd59
bruno <bweisz@eryxsoluciones.com.ar>
2024-07-15 12:44:20 -0300

    Tests for majority function

--- plonky2-backend/src/circuit_translation/tests/test_sha256.rs
+++ plonky2-backend/src/circuit_translation/tests/test_sha256.rs
@@ -170,6 +170,76 @@ fn test_choose_32(){
     test_choose(32, chooser, inputs_1, inputs_2, outputs);
 }
 
+#[test]
+fn test_majority_4(){
+    let g_zero = F::default();
+    let g_one = F::from_canonical_u32(1);
+    let inputs_0 = vec![g_zero, g_one, g_zero, g_one];
+    let inputs_1 = vec![g_one, g_one, g_zero, g_zero];
+    let inputs_2 = vec![g_zero, g_zero, g_one, g_one];
+    let outputs = vec![g_zero, g_one, g_zero, g_one];
+    test_majority(4, inputs_0, inputs_1, inputs_2, outputs);
+}
+
+#[test]
+#[should_panic]
+fn test_majority_4_failed(){
+    let g_zero = F::default();
+    let g_one = F::from_canonical_u32(1);
+    let inputs_0 = vec![g_zero, g_one, g_zero, g_one];
+    let inputs_1 = vec![g_one, g_one, g_zero, g_zero];
+    let inputs_2 = vec![g_zero, g_zero, g_one, g_one];
+    let outputs = vec![g_zero, g_one, g_zero, g_zero];
+    test_majority(4, inputs_0, inputs_1, inputs_2, outputs);
+}
+
+#[test]
+fn test_majority_32(){
+    let g_zero = F::default();
+    let g_one = F::from_canonical_u32(1);
+    let inputs_0 = vec![
+        g_zero, g_zero, g_zero, g_zero, g_zero, g_zero, g_zero, g_zero,
+        g_zero, g_zero, g_zero, g_zero, g_zero, g_zero, g_zero, g_zero,
+        g_zero, g_zero, g_zero, g_zero, g_zero, g_zero, g_zero, g_zero,
+        g_zero, g_zero, g_zero, g_zero, g_zero, g_zero, g_zero, g_zero,
+    ];
+    let inputs_1 = inputs_0.clone();
+    let inputs_2 = vec![
+        g_one, g_one, g_one, g_one, g_one, g_one, g_one, g_one,
+        g_one, g_one, g_one, g_one, g_one, g_one, g_one, g_one,
+        g_one, g_one, g_one, g_one, g_one, g_one, g_one, g_one,
+        g_one, g_one, g_one, g_one, g_one, g_one, g_one, g_one,
+    ];
+    let outputs = inputs_0.clone();
+    test_majority(32, inputs_0, inputs_1, inputs_2, outputs);
+}
+
+
+fn test_majority(size: usize, input_values_0: Vec<F>, input_values_1: Vec<F>, input_values_2: Vec<F>, output_values: Vec<F>){
+    let config = CircuitConfig::standard_recursion_config();
+    let mut circuit_builder = CB::new(config);
+
+    let bits_0 = (0..size).into_iter().map(|_| circuit_builder.add_virtual_bool_target_unsafe()).collect();
+    let bits_1 = (0..size).into_iter().map(|_| circuit_builder.add_virtual_bool_target_unsafe()).collect();
+    let bits_2 = (0..size).into_iter().map(|_| circuit_builder.add_virtual_bool_target_unsafe()).collect();
+
+    let binary_input_0 = BinaryDigitsTarget{bits: bits_0};
+    let binary_input_1 = BinaryDigitsTarget{bits: bits_1};
+    let binary_input_2 = BinaryDigitsTarget{bits: bits_2};
+    let chosen_bits = BinaryDigitsTarget::majority(&binary_input_0, &binary_input_1, &binary_input_2, &mut circuit_builder);
+
+    let mut partial_witnesses = PartialWitness::<F>::new();
+    for i in 0..size {
+        partial_witnesses.set_target(binary_input_0.bits[i].target, input_values_0[i]);
+        partial_witnesses.set_target(binary_input_1.bits[i].target, input_values_1[i]);
+        partial_witnesses.set_target(binary_input_2.bits[i].target, input_values_2[i]);
+        partial_witnesses.set_target(chosen_bits.bits[i].target, output_values[i]);
+    }
+
+    let circuit_data = circuit_builder.build::<C>();
+    let proof = circuit_data.prove(partial_witnesses).unwrap();
+    assert!(circuit_data.verify(proof).is_ok());
+}
 
 fn test_choose(size: usize, chooser_values: Vec<F>, input_values_1: Vec<F>, input_values_2: Vec<F>, output_values: Vec<F>){
     let config = CircuitConfig::standard_recursion_config();

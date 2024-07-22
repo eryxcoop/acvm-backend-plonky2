a19e623f41c783542783cbe96f8a96d4a3436457
bruno <bweisz@eryxsoluciones.com.ar>
2024-07-22 10:57:34 -0300

    wip

--- plonky2-backend/src/main.rs
+++ plonky2-backend/src/main.rs
@@ -64,7 +64,8 @@ fn main() {
         prove_command_name,
         vec![
             _prove_argument_circuit_path(),
-            _prove_argument_witness_path()
+            _prove_argument_witness_path(),
+            _prove_argument_output_path()
         ]
     );
 
@@ -79,18 +80,11 @@ fn main() {
             _prove_argument_circuit_path().get_id().to_string().as_str()).expect("---");
         let witness_path = subcommand_matches.get_one::<PathBuf>(
             _prove_argument_witness_path().get_id().to_string().as_str()).expect("---");
+        let output_path = subcommand_matches.get_one::<PathBuf>(
+            _prove_argument_output_path().get_id().to_string().as_str()).expect("---");
 
-        println!("{:?} {:?}", circuit_path, witness_path);
+        _execute_prove_command(circuit_path, witness_path, output_path);
     }
-
-    // let args: Vec<String> = env::args().collect();
-    // let command: &str = &args.get(1).expect("Must specify a command");
-    // match command {
-    //     "prove" => _execute_prove_command(&args),
-    //     "write_vk" => _execute_write_vk_command(&args),
-    //     "verify" => _execute_verify_command(&args),
-    //     other => eprintln!("Invalid command: {:?}", other),
-    // }
 }
 
 fn _prove_argument_circuit_path() -> Arg {
@@ -125,18 +119,29 @@ fn _prove_argument_witness_path() -> Arg {
     circuit_path_argument
 }
 
+fn _prove_argument_output_path() -> Arg {
+    let circuit_path_argument_id = "output_path";
+    let short_command_identifier = 'o';
+    let long_command_identifier = "output-path";
+    let short_help_circuit_argument = "Path where the generated proof is to be stored";
+    let long_help_circuit_argument = "";
+    let circuit_path_argument = create_argument(
+        circuit_path_argument_id,
+        short_command_identifier,
+        long_command_identifier,
+        short_help_circuit_argument,
+        long_help_circuit_argument,
+    );
+    circuit_path_argument
+}
 
 
-fn _execute_prove_command(args: &Vec<String>) {
-    let acir_program_json_path = &args[3];
-    let witness_stack_zip_path = &args[5];
-    let resulting_proof_file_path = &args[7];
-    let prove_action = actions::prove_action::ProveAction {
-        acir_program_json_path: acir_program_json_path.clone(),
-        witness_stack_zip_path: witness_stack_zip_path.clone(),
-        resulting_proof_file_path: resulting_proof_file_path.clone(),
-    };
-    prove_action.run();
+fn _execute_prove_command(circuit_path: &PathBuf, witness_path: &PathBuf, output_path: &PathBuf) {
+    actions::prove_action::ProveAction {
+        acir_program_json_path: String::from(circuit_path.to_str().unwrap()),
+        witness_stack_zip_path: String::from(witness_path.to_str().unwrap()),
+        resulting_proof_file_path: String::from(output_path.to_str().unwrap()),
+    }.run();
 }
 
 fn _execute_write_vk_command(args: &Vec<String>) {
